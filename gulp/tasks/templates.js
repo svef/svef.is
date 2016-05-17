5fcbf2fb6324c7ad89e131ac5a4bd972a966e755
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-05-17 23:04:13 +0000

    Add basedir to pug compile for absolute paths

--- gulp/tasks/templates.js
+++ gulp/tasks/templates.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const gulp = require('gulp')
 const pug = require('gulp-pug')
 const config = require('../config')
@@ -7,6 +8,7 @@ gulp.task('templates',
   () => gulp.src(`${config.source}/templates/**/*.pug`)
     .pipe(pug({
       pretty: config.minify,
+      basedir: path.join(process.cwd(), 'src'),
       locals: {
         // content will be injected here
       },
