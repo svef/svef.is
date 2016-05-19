b9df00bbc42236b2a5621cfe4a6766d8708c9ec1
Ægir Giraldo Þorsteinsson <aegir@thorsteinsson.is>
2016-05-19 20:27:49 +0000

    Add slack invite

--- src/scripts/routes/index.js
+++ src/scripts/routes/index.js
@@ -1,5 +1,17 @@
+import requests from '../modules/requests'
+
 export default {
   load: () => {
-    console.log('index loaded')
+    const form = document.getElementById('slack-invite')
+    form.addEventListener('submit', (event) => {
+      event.preventDefault()
+
+      const email = document.getElementById('slack-email').value
+      requests.post(`${apiUrl}/slack`, { email }).then((result) => {
+        if (result.ok) {
+          form.remove()
+        }
+      })
+    })
   },
 }
