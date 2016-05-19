c4c0b3a57d21f2ce445d29b119a794df9b0215c8
Aegir Giraldo Thorsteinsson <aegir@thorsteinsson.is>
2016-05-19 20:27:20 +0000

    Get medium posts (#8)

--- src/scripts/routes/index.js
+++ src/scripts/routes/index.js
@@ -1,5 +1,18 @@
+import requests from '../modules/requests'
+
+const apiUrl = 'http://api.svef.is'
+const postTemplate = (post) => `
+  <div class="post">
+    <h2><a href="${post.url}">${post.title}</a></h2>
+    <p>${post.content}</p>
+  </div>
+`
+
 export default {
   load: () => {
-    console.log('index loaded')
+    requests.get(`${apiUrl}/posts`).then((posts) => {
+      const markup = JSON.parse(posts).map(postTemplate)
+      document.getElementById('posts').innerHTML = markup
+    })
   },
 }
